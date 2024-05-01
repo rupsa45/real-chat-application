365ca670cda92849c65175d42c3dc67c6f3d05d2
rupsa45 <drupsa767@gmail.com>
2024-05-01 16:01:47 +0530

    first commit

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ const UserState={
 
 const io=new Server(expressServer,{
     cors:{
-        origin:process.env.NODE_ENV === "production" ? false : ["http://localhost:3000",
-        "http://127.0.0.1:3001"],
+        origin:process.env.NODE_ENV === "production" ? false : ["https://real-chat-application-dmlq.onrender.com"],
     }
 })
 
